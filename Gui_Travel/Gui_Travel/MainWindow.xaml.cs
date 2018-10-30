9e78a64fc2f5e554823d0df155e986cb9fb0105b
philipmetzgercsbe <philip.metzger@csbe.ch>
2018-10-30 11:44:48 +0100

    Changes

---
+++ Gui_Travel/Gui_Travel/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Gui_Travel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            registrationControl registrationControl = new registrationControl();
+            loginControl loginControl = new loginControl();
+            this.AddChild(loginControl);
+            //If your logged in display Hotels & User Controls
+
+            
+        }
+    }
+}
